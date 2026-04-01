facff92241b2a9ae72a0f98c46c7e1e702d8481f
刘正扬 <liuzybj@outlook.com>
2026-04-01 20:26:23 +0800

    Initial commit. App init. complete.

---
+++ src/main/java/org/example/lifecomposer/LifeComposerApplication.java
@@ -0,0 +1,13 @@
+package org.example.lifecomposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeComposerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeComposerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lifecomposer/LifeComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lifecomposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeComposerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
